95845a7759d9f02601f9b1542879df06dba6a7b6
Indra Gunawan <go.indra@icloud.com>
2020-06-14 17:37:00 +0700

    Initial commit

---
+++ src/bootstrap/framework7.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import 'framework7/css/framework7.bundle.min.css';
+// import 'framework7/css/framework7.css';
+
+import Framework7 from 'framework7/framework7-lite.esm.bundle.js';
+// import Framework7 from 'framework7/framework7.esm.js';
+import Framework7React from 'framework7-react';
+
+Framework7.use(Framework7React);
+
+import { App } from 'framework7-react';
+
+const f7params = {
+  // Array with app routes
+
+  // routes,
+  theme: import.meta.env.SNOWPACK_PUBLIC_THEME || 'auto',
+
+  // App Name
+  name: import.meta.env.SNOWPACK_PUBLIC_APPNAME || 'Framework7 - Snowpack',
+
+  // App id
+  id: import.meta.env.SNOWPACK_PUBLIC_APPID || 'id.co.qumatic.app',
+  // ...
+};
+
+export default function (props) {
+  return (
+    <App params={f7params}>
+      {props.children}
+    </App>
+  )
+}
